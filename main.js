a9be2fe568bf5fd024496d0209c9502523015ae5
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-05-03 21:41:55 +0200

    Correzioni generali

--- main.js
+++ main.js
@@ -1,70 +1,54 @@
-//Chiedere il nome all'utente
-var userName = prompt('Inserisci il tuo nome');
-document.getElementById('user_name').innerHTML = userName;
-
-
-// Chiedere all'utente il numero di chilometri che vuole percorrere 
-var kilometers = parseInt(prompt('Inserire il numero di chilometri da percorrere'));
-console.log(kilometers);
-document.getElementById('distance').innerHTML = kilometers;
-
-if (isNaN(kilometers)) {
-    alert('Il valore inserito non è valido! Inserire un numero.')
-}
-
-
-// Chiedere l'età del passeggero.
-/* var userAge = parseInt(prompt("Inserire l'età"));
-console.log(userAge);
-document.getElementById('age').innerHTML = 'Età del passeggero: ' + userAge;
-
-if (isNaN(userAge)) {
-    alert('Il valore inserito non è valido! Inserire un numero.')
-} */
-
-/* 
-// Clcolare il prezzo totale del viaggio
-var priceForKm = 0.21;
-var ticketPrice = kilometers * priceForKm;
-console.log(ticketPrice);
-document.getElementById('price').innerHTML = 'Il costo del biglietto è: ' + ticketPrice.toFixed(2) + ' \u20ac';
-var result;
-document.getElementById('result').innerHTML = 'Il prezzo è ' + ticketPrice.toFixed(2) + ' \u20ac';
-
-if (userAge < 18 ) {
-    var sconto = (ticketPrice*20)/100;
-    document.getElementById('sconto').innerHTML = 'Con lo sconto del 20% rispami: ' + sconto.toFixed(2) + ' \u20ac';
-    console.log(sconto);
-    var teenPrice = ticketPrice - sconto;
-    document.getElementById('final_price').innerHTML = 'Il costo finale del biglietto è: ' + teenPrice.toFixed(2) + ' \u20ac';
-    console.log(teenPrice);
-    var result;
-    document.getElementById('result').innerHTML = 'Il prezzo è ' + teenPrice.toFixed(2) + ' \u20ac';
-
-} else if (userAge >= 65) {
-    var sconto = (ticketPrice*40)/100;
-    document.getElementById('sconto').innerHTML = 'Con lo sconto del 40% risparmi: ' + sconto.toFixed(2) + ' \u20ac';
-    console.log(sconto);
-    var overPrice = ticketPrice - sconto;
-    document.getElementById('final_price').innerHTML = 'Il costo finale del biglietto è: ' + overPrice.toFixed(2) + ' \u20ac';
-    console.log(overPrice);
-    var result;
-    document.getElementById('result').innerHTML = 'Il prezzo è ' + overPrice.toFixed(2) + ' \u20ac';
-}
- */
-
-//Generare il numero di carrozza
-var numberCarriage = Math.floor(Math.random() * 10) + 1;
-document.getElementById('number_carriage').innerHTML = numberCarriage;
-
-//Generare il codice CP
-var numerbCode = Math.floor(Math.random() * 100000) + 90000;
-document.getElementById('number_code').innerHTML = numerbCode;
-
-//Geneare un evento al click sui button
 var btnAnnulla = document.getElementById('btn-annulla');
 
 btnAnnulla.addEventListener('click', function() {
     var userNameInput = document.getElementById('user_name');
     userNameInput.value = "";
+    var userDistance = document.getElementById('distance');
+    userDistance.value = "";
+    var userAge = document.getElementById('age');
+    userAge.value = "";
 })
+
+
+var btnGenera = document.getElementById('btn-genera');
+
+btnGenera.addEventListener('click', function() {
+    var nomeUtenteEl = document.getElementById("user_name").value; 
+    document.getElementById('recup_name').innerHTML = nomeUtenteEl;
+
+    var ageE1 = document.getElementById('age').value;
+    document.getElementById('sales').innerHTML = 'Sconto' + '<br></br>' + ageE1;
+
+    var numberCarriage = Math.floor(Math.random() * 10) + 1;
+    document.getElementById('number_carriage').innerHTML = numberCarriage;  
+
+    var numberCode = Math.floor(Math.random() * (100000 - 90000) ) + 90000;
+    document.getElementById('number_code').innerHTML = numberCode;
+    
+    var priceForKm = 0.21;
+    var ticketPrice = distance.value * priceForKm;
+    console.log(ticketPrice);
+    
+    if(age.value == 'Minorenne'){
+        var sconto = (ticketPrice*20)/100;
+        console.log('Lo sconto è: ' + sconto);
+        var teenPrice = ticketPrice - sconto;
+        document.getElementById('price').innerHTML = teenPrice.toFixed(2) + ' \u20ac';
+    } else if (age.value == 'Anziano') {
+        var sconto = (ticketPrice*40)/100;
+        console.log('Lo sconto è: ' + sconto);
+        var overPrice = ticketPrice - sconto;
+        document.getElementById('price').innerHTML = overPrice.toFixed(2) + ' \u20ac';
+    } else if (age.value == 'Maggiorenne') {
+       
+        document.getElementById('price').innerHTML = ticketPrice.toFixed(2) + ' \u20ac';
+    }
+})
+
+
+
+
+
+
+
+
